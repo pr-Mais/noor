1c0991e7450bd9f3f99147a5fae6e865a18b5aa4
pr_Mais <hiimechi@gmail.com>
2021-05-12 08:05:30 +0300

    Animating splash logo entrance

--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
-import 'package:provider/provider.dart';
 
 import 'package:noor/exports/pages.dart' show RootHome;
-import 'package:noor/exports/controllers.dart' show DataController;
 
 class SplashScreen extends StatefulWidget {
   SplashScreen({Key key}) : super(key: key);
@@ -13,10 +11,21 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  double logoOpacity = 0.0;
+  @override
+  void initState() {
+    Future<void>.delayed(Duration(milliseconds: 300), () {
+      setState(() {
+        logoOpacity = 1.0;
+      });
+    });
+    super.initState();
+  }
+
   @override
   void didChangeDependencies() async {
     await Future<void>.value(GetIt.I.allReady());
-    await Future<void>.delayed(Duration(seconds: 1));
+    await Future<void>.delayed(Duration(milliseconds: 500));
     Navigator.of(context).pushReplacement(
       MaterialPageRoute<RootHome>(
         builder: (_) => RootHome(),
@@ -28,22 +37,14 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedSwitcher(
-      duration: Duration(microseconds: 800),
-      child: Material(
-        child: Center(
-          child: AnimatedSwitcher(
-            transitionBuilder: (Widget child, Animation<double> animation) => AnimatedOpacity(
-              opacity: animation.value,
-              duration: Duration(milliseconds: 100),
-              child: child,
-            ),
-            child: Image.asset(
-              'assets/NoorLogo.png',
-              width: 150,
-              key: ValueKey<String>('logo'),
-            ),
-            duration: Duration(milliseconds: 100),
+    return Material(
+      child: Center(
+        child: AnimatedOpacity(
+          duration: Duration(milliseconds: 500),
+          opacity: logoOpacity,
+          child: Image.asset(
+            'assets/NoorLogo.png',
+            width: 150,
           ),
         ),
       ),
