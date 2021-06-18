9021ecbe48351459ba3ad4b27b003c67df6f7264
pr_Mais <hiimechi@gmail.com>
2021-06-18 20:27:38 +0300

    Adding 0670 unicode to tones list (Tashkeel)

--- lib/utils/remove_tashkeel.dart
+++ lib/utils/remove_tashkeel.dart
@@ -2,7 +2,7 @@ class Tashkeel {
   static remove(String string) {
     String tmp = string.replaceAll(
         RegExp(
-          r'[\u{0618}\u{0619}\u{061A}\u{064B}\u{064C}\u{064D}\u{064E}\u{064F}\u{0650}\u{0651}\u{0652}\u{0653}\u{0651}]',
+          r'[\u{0618}\u{0619}\u{061A}\u{064B}\u{064C}\u{064D}\u{064E}\u{064F}\u{0650}\u{0651}\u{0652}\u{0653}\u{0651}\u{0670}]',
           unicode: true,
         ),
         '');
